8386de168f637a354d98a8afa3a778bb052041cf
Xawe <dreamxawe@hotmail.com>
2022-01-25 22:05:24 -0300

    Setting project

---
+++ blog_writer_api/lib/blog_writer_api.ex
@@ -0,0 +1,18 @@
+defmodule BlogWriterApi do
+  @moduledoc """
+  Documentation for `BlogWriterApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BlogWriterApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
