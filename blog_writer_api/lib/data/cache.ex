023f0ad505167b86f7da3f0235512361a76d36cc
Xawe <dreamxawe@hotmail.com>
2022-01-28 23:05:49 -0300

    implementação de GenServer Cache

--- blog_writer_api/lib/data/cache.ex
+++ blog_writer_api/lib/data/cache.ex
@@ -2,7 +2,18 @@ defmodule Data.Cache do
   def use_redix(data) do
     {:ok, conn} = Redix.start_link("redis://172.18.0.2:6379", name: :redix)
     result = Redix.command(conn, ["SADD", "post_cache", data])
-    # result = Redix.command(conn, ["SET", "mykey", "foo"])
     result
   end
+
+  def set(key, value) do
+    Data.CacheServer.sadd(key, value)
+  end
+
+  def exists_set(key, value) do
+    Data.CacheServer.sismember({key, value})
+    |> exists()
+  end
+
+  defp exists(1), do: :ok
+  defp exists(0), do: :none
 end
