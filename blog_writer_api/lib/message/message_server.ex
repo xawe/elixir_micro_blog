2f953c486a976ed0eeba183e60d7da5e67f42385
Xawe <dreamxawe@hotmail.com>
2022-02-07 12:16:45 -0300

    format only

--- blog_writer_api/lib/message/message_server.ex
+++ blog_writer_api/lib/message/message_server.ex
@@ -2,16 +2,15 @@ defmodule Message.MessageServer do
   use GenServer
   require Logger
 
-def start_link(_) do
-  GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
-end
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
 
-def init(_) do
-  {:ok, connection} = AMQP.Connection.open()
-  Logger.info("Starting AMQP Connection")
-  {:ok, channel} = AMQP.Channel.open(connection)
-  Logger.info(channel)
-  {:ok, channel}
-end
-
+  def init(_) do
+    {:ok, connection} = AMQP.Connection.open()
+    Logger.info("Starting AMQP Connection")
+    {:ok, channel} = AMQP.Channel.open(connection)
+    Logger.info(channel)
+    {:ok, channel}
+  end
 end
