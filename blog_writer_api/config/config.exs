9072817322839842f212d8d5c8b7d4e1690fbf44
Xawe <dreamxawe@hotmail.com>
2022-02-07 22:46:14 -0300

    Iniciando implementações de fila
    Criada configuração para conexão local com Rabbit
    
    Criado genServer de consumidor ( template --- WIP ) para testes

--- blog_writer_api/config/config.exs
+++ blog_writer_api/config/config.exs
@@ -5,4 +5,12 @@ config :app,
   redis_host: "172.24.0.2",
   redis_port: 6379
 
+config :amqp,
+  connections: [
+    myconn: [url: "amqp://guest:guest@172.24.0.3:5672"],
+  ],
+  channels: [
+    mychan: [connection: :myconn]
+  ]
+
 import_config "#{Mix.env()}.exs"
