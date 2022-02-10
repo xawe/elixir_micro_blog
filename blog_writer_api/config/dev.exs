b92411fa3132078b77c45715aa20b56d49b53bb7
Xawe <dreamxawe@hotmail.com>
2022-02-09 22:22:25 -0300

    ajuste em configs para evitar erro de conexao no perfil dev

--- blog_writer_api/config/dev.exs
+++ blog_writer_api/config/dev.exs
@@ -2,22 +2,4 @@ import Config
 
 # Development configurations goes here
 config :app,
-  port: 8000,
-  redis_host: "red-cache",
-  redis_port: 6379
-
-
-  config :amqp,
-  connections: [
-    msg_conn: [url: "amqp://guest:guest@red-rabbit:5672"],
-  ],
-  channels: [
-    msg_channel: [
-      connection: :msg_conn,
-    ]
-  ]
-# config :app ,
-# redis_host: "redis://red-cache"
-
-# Example of database configuration
-# config :app, db_config: %{name: "dev_db", password: "", port: 10000}
+  port: 8000
