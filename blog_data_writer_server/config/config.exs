2eb78cf6aba29f41c4b420a055128ba2770bc1d3
Xawe <dreamxawe@hotmail.com>
2022-02-21 21:40:58 -0300

    .

--- blog_data_writer_server/config/config.exs
+++ blog_data_writer_server/config/config.exs
@@ -1,3 +1,9 @@
 import Config
 
+config :app, Friends.Repo,
+  database: "blog_data_writer",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
 import_config "#{Mix.env()}.exs"
---
+++ blog_data_writer_server/lib/app/repo.ex
@@ -0,0 +1,5 @@
+defmodule App.Repo do
+  use Ecto.Repo,
+    otp_app: :friends,
+    adapter: Ecto.Adapters.Postgres
+end
