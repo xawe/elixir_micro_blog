07e07a733c31c9d689c052b6230b4cd41c112014
Xawe <dreamxawe@hotmail.com>
2022-03-07 22:42:35 -0300

    implementação padrão de consumer. WIP - Necessário refatorar usando as variaveis e filas criadas via config

--- blog_data_writer_server/lib/message/consumer.ex
+++ blog_data_writer_server/lib/message/consumer.ex
@@ -1,4 +1,72 @@
 defmodule Message.Consumer do
-  # use GenServer
+  use GenServer
+  use AMQP
   require Logger
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], [])
+  end
+
+  @exchange "gen_server_test_exchange"
+  @queue "gen_server_test_queue"
+  @queue_error "#{@queue}_error"
+
+  def init(_opts) do
+    {:ok, conn} = Connection.open("amqp://guest:guest@localhost")
+    {:ok, chan} = Channel.open(conn)
+    setup_queue(chan)
+
+    :ok = Basic.qos(chan, prefetch_count: 10)
+    {:ok, _consumer_tag} = Basic.consume(chan, @queue)
+    {:ok, chan}
+  end
+
+  def handle_info({:basic_consume_ok, %{consumer_tag: consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  def handle_info({:basic_cancel, %{consumer_tag: consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  def handle_info({:basic_cancel_ok, %{consumer_tag: consumer_tag}}, chan) do
+    {:noreply, chan}
+  end
+
+  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, chan) do
+    consume(chan, tag, redelivered, payload)
+    {:noreply, chan}
+  end
+
+  defp setup_queue(chan) do
+    {:ok, _} = Queue.declare(chan, @queue_error, durable: true)
+
+    {:ok, _} =
+      Queue.declare(chan, @queue,
+        durable: true,
+        arguments: [
+          {"x-dead-letter-exchange", :longstr, ""},
+          {"x-dead-letter-routing-key", :longstr, @queue_error}
+        ]
+      )
+
+    :ok = Exchange.fanout(chan, @exchange, durable: true)
+    :ok = Queue.bind(chan, @queue, @exchange)
+  end
+
+  defp consume(channel, tag, redelivered, payload) do
+    number = String.to_integer(payload)
+
+    if number <= 10 do
+      :ok = Basic.ack(channel, tag)
+      IO.puts("Consumed a #{number}.")
+    else
+      :ok = Basic.reject(channel, tag, requeue: false)
+      IO.puts("#{number} is too big and was rejected.")
+    end
+  rescue
+    exception ->
+      :opk = Basic.reject(channel, tag, requeue: not redelivered)
+      IO.puts("Error converting #{payload} to integer")
+  end
 end
