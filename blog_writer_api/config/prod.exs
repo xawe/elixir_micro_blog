faa895511e9926c74bbf38e6dcceb1152858675b
Xawe <dreamxawe@hotmail.com>
2022-03-22 22:48:32 -0300

    ajuste de config de produção

--- blog_writer_api/config/prod.exs
+++ blog_writer_api/config/prod.exs
@@ -5,14 +5,17 @@ config :app,
   port: 8000,
   # redis_host: "redis://172.24.0.2:6379",
   redis_host: "red-cache",
-  redis_port: 6379
+  redis_port: 6379,
+  exchange: "post_data_ex",
+  queue: "post_data_ok",
+  error_queue: "post_data_error"
 
-# config :amqp,
-# connections: [
-#   msg_conn: [url: "amqp://guest:guest@red-rabbit:5672"],
-# ],
-# channels: [
-#   msg_channel: [
-#     connection: :msg_conn,
-#   ]
-# ]
+config :amqp,
+  connections: [
+    msg_conn: [url: "amqp://guest:guest@red-rabbit:5672"]
+  ],
+  channels: [
+    msg_channel: [
+      connection: :msg_conn
+    ]
+  ]
